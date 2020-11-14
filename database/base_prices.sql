f3f4de3a5f3a65ae42df714c47f406a6c1454222
CowSaysBaa <mememe479@gmail.com>
2020-11-14 19:51:28 +0800

    Added sql to add data to base_prices table.

---
+++ database/base_prices.sql
@@ -0,0 +1,8 @@
+insert into base_prices (category, price) values ('Cat', 5);
+insert into base_prices (category, price) values ('Dog', 10);
+insert into base_prices (category, price) values ('Hamster', 2);
+insert into base_prices (category, price) values ('Fish', 1);
+insert into base_prices (category, price) values ('Bird', 3);
+insert into base_prices (category, price) values ('Turtle', 10);
+insert into base_prices (category, price) values ('Monkey', 20);
+insert into base_prices (category, price) values ('Tiger', 100);
